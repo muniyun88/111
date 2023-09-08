8d7e2162e178a5500c5dd55e17df2d042768e4e2
muniyun <44489924+muniyun@users.noreply.github.com>
2023-09-08 21:44:23 +0800

    aa

---
+++ top.js
@@ -0,0 +1,26 @@
+document.write('<table width="980" height="90" border="0" align=center cellpadding="0" cellspacing="0" background="img/logo1.gif">');
+document.write('<tr>');
+document.write('<td width="205">');
+document.write('<div align=center>');
+document.write('<img src="img/logo.gif" width="260" height="90">');
+document.write('</div>');
+document.write('</td>');
+document.write('<td width="775" align=middle>');
+document.write('<div align="center">');
+document.write('</div>');
+document.write('<div align="center">');
+document.write('<TABLE  width="720" border="0">');
+document.write('<tr>');
+document.write('<td>');
+document.write('<div align=center>');
+document.write('<font face="微软雅黑"  size="3"><a href="../">首&nbsp;&nbsp;页</a>&nbsp;&nbsp;<a href="../fo/jdjs/">寺院介绍</a>&nbsp;&nbsp;<a href="../fo/zaixianliuyan/">住持介绍</a>&nbsp;&nbsp;<a href="../fo/news/">佛教新闻</a>&nbsp;&nbsp;<a href="../fo/lxtj/">佛学文化</a>&nbsp;&nbsp;<a href="../fo/tsms/">寺院巡礼</a>&nbsp;&nbsp;<a href="../fo/kfzs/">高僧大德</a>&nbsp;&nbsp;<a href="../fo/fuxuexiazai/">佛学下载</a>&nbsp;&nbsp;<a href="../fo/lianxiwomen/">联系我们</a></font>');
+document.write('</div>');
+document.write('</TD>');
+document.write('</tr>');
+document.write('</table>');
+document.write('</div>');
+document.write('</td>');
+document.write('</tr>');
+document.write('</table>');
+
+
